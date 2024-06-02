b35f0eaed16aacee3b949a43939ea3d608909d30
DylanB <dylanbergmann25@gmail.com>
2024-06-02 07:10:34 +0700

    switch producer of topic and direct exchange

--- python/advanced/rabbitmq/direct_exchange/producer.py
+++ python/advanced/rabbitmq/direct_exchange/producer.py
@@ -2,17 +2,17 @@ import pika
 from pika.exchange_type import ExchangeType
 
 connection_parameters = pika.ConnectionParameters('localhost')
+
 connection = pika.BlockingConnection(connection_parameters)
+
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic', exchange_type=ExchangeType.topic)
+channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 
-user_payments = "A european user pays for something"
-channel.basic_publish(exchange='topic', routing_key='user.europe.payments', body=user_payments)
-print(f"sent message: {user_payments}")
+message = "This message needs to be routed"
 
-business_order = "A european business ordered goods"
-channel.basic_publish(exchange='topic', routing_key='business.europe.order', body=business_order)
-print(f"sent message: {business_order}")
+channel.basic_publish(exchange='routing', routing_key='both', body=message)
+
+print(f"sent message: {message}")
 
 connection.close()
